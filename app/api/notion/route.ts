e6294cec91d8b6d6ea32a46ed8f08f97c7a4f9c6
Aodhan Hamilton <hamilton.aodhan@gmail.com>
2023-09-30 23:34:24 +1300

    edit notion api key

--- app/api/notion/route.ts
+++ app/api/notion/route.ts
@@ -5,22 +5,16 @@ export async function GET() {
     const pageId = '618ebcb981fe49359dbb9d5e93ece114'
     const res = await fetch(`https://api.notion.com/v1/databases/${databaseId}/query`, {
         method: 'POST',
-        headers:{
-            'Authorization': `Bearer ${process.env.Notion_API_KEY}`,
+        headers: {
+            'Authorization': `Bearer ${process.env.NOTION_API_KEY}`,
             'Notion-Version': '2022-06-28',
             'Content-Type': 'application/json'
         }
     });
-    // const res = await fetch(`https://api.notion.com/v1/pages/${pageId}`, {
-    //     headers:{
-    //         'Authorization': `Bearer ${process.env.Notion_API_KEY}`,
-    //         'Notion-Version': '2022-06-28',
-    //         'Content-Type': 'application/json'
-    //     }
-    // });
-  
-    const data = await res.json();
-   
 
-  return NextResponse.json(data);
+
+    const data = await res.json();
+
+
+    return NextResponse.json(data);
 }
