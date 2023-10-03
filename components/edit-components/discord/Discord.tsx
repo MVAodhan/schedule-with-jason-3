a6d3363145d5444822a70755d48132fa8240893f
Aodhan Hamilton <hamilton.aodhan@gmail.com>
2023-10-04 00:04:47 +1300

    ft: add copy without guest fn

--- components/edit-components/discord/Discord.tsx
+++ components/edit-components/discord/Discord.tsx
@@ -6,90 +6,97 @@ import { Episode } from "@prisma/client";
 import { getEndDate } from "@/lib/my-utils";
 
 const Discord = ({
-	episode,
-	nzDate,
-	guest,
+  episode,
+  nzDate,
+  guest,
 }: {
-	episode: Episode;
-	nzDate: string;
-	guest: Guest;
+  episode: Episode;
+  nzDate: string;
+  guest: Guest;
 }) => {
-	const titleRef = useRef<HTMLInputElement | null>(null);
-	const descRef = useRef<HTMLTextAreaElement | null>(null);
-	const endDate = getEndDate(episode.date);
-	const poster = `https://www.learnwithjason.dev/${episode.slug}/poster.jpg`;
+  const titleRef = useRef<HTMLInputElement | null>(null);
+  const descRef = useRef<HTMLTextAreaElement | null>(null);
+  const endDate = getEndDate(episode.date);
+  const poster = `https://www.learnwithjason.dev/${episode.slug}/poster.jpg`;
 
-	const copyValue = (ref: any) => {
-		if (ref.current?.value !== null) {
-			const string = `LWJ: ${ref.current?.value} with ${guest.name}`;
-			navigator.clipboard.writeText(string);
-		}
-	};
+  const copyValue = (ref: any) => {
+    if (ref.current?.value !== null) {
+      const string = `LWJ: ${ref.current?.value} with ${guest.name}`;
+      navigator.clipboard.writeText(string);
+    }
+  };
 
-	function downloadPoster() {
-		const a = document.createElement("a");
-		a.href = poster;
-		a.download = "poster.jpg";
-		document.body.appendChild(a);
-		a.click();
-		document.body.removeChild(a);
-	}
+  const copyWithoutGuest = (ref: any) => {
+    if (ref.current?.value !== null) {
+      const string = ref.current?.value;
+      navigator.clipboard.writeText(string);
+    }
+  };
 
-	return (
-		<div className="w-full flex flex-col items-center">
-			<div className=" flex justify-center items-center">
-				Location
-				<VscCopy
-					className="cursor-pointer pl-1 h-8 w-8"
-					onClick={() => {
-						navigator.clipboard.writeText("https://twitch.tv/jlengstorf");
-					}}
-				/>
-			</div>
-			<div className="flex items-center w-4/5 justify-between mt-5 ">
-				<input
-					type="text"
-					defaultValue={episode.title}
-					ref={titleRef}
-					className="input input-bordered bg-slate-50 w-full"
-				/>
-				<VscCopy
-					className="cursor-pointer pl-1 h-8 w-8"
-					onClick={() => {
-						copyValue(titleRef);
-					}}
-				/>
-			</div>
+  function downloadPoster() {
+    const a = document.createElement("a");
+    a.href = poster;
+    a.download = "poster.jpg";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
 
-			<div className="flex items-center w-4/5 justify-between m-2">
-				<div>Start Time:</div>
-				<div>{nzDate ?? nzDate}</div>
-			</div>
-			<div className="flex items-center w-4/5 justify-between m-2 ">
-				<div>End Time:</div>
-				<div>{episode.date ? endDate : ""}</div>
-			</div>
+  return (
+    <div className="w-full flex flex-col items-center">
+      <div className=" flex justify-center items-center">
+        Location
+        <VscCopy
+          className="cursor-pointer pl-1 h-8 w-8"
+          onClick={() => {
+            navigator.clipboard.writeText("https://twitch.tv/jlengstorf");
+          }}
+        />
+      </div>
+      <div className="flex items-center w-4/5 justify-between mt-5 ">
+        <input
+          type="text"
+          defaultValue={episode.title}
+          ref={titleRef}
+          className="input input-bordered bg-slate-50 w-full"
+        />
+        <VscCopy
+          className="cursor-pointer pl-1 h-8 w-8"
+          onClick={() => {
+            copyValue(titleRef);
+          }}
+        />
+      </div>
 
-			<div className="flex items-center w-4/5 justify-between m-2 ">
-				<textarea
-					className="textarea textarea-bordered w-full bg-slate-50"
-					defaultValue={episode.description}
-					ref={descRef}
-				></textarea>
-				<VscCopy
-					className="cursor-pointer pl-1 h-8 w-8"
-					onClick={() => {
-						copyValue(descRef);
-					}}
-				/>
-			</div>
-			<div className="mt-5 flex justify-center">
-				<a href={poster} target="_blank">
-					Poster
-				</a>
-			</div>
-		</div>
-	);
+      <div className="flex items-center w-4/5 justify-between m-2">
+        <div>Start Time:</div>
+        <div>{nzDate ?? nzDate}</div>
+      </div>
+      <div className="flex items-center w-4/5 justify-between m-2 ">
+        <div>End Time:</div>
+        <div>{episode.date ? endDate : ""}</div>
+      </div>
+
+      <div className="flex items-center w-4/5 justify-between m-2 ">
+        <textarea
+          className="textarea textarea-bordered w-full bg-slate-50"
+          defaultValue={episode.description}
+          ref={descRef}
+        ></textarea>
+        <VscCopy
+          className="cursor-pointer pl-1 h-8 w-8"
+          onClick={() => {
+            copyWithoutGuest(descRef);
+          }}
+        />
+      </div>
+      <div className="mt-5 flex justify-center">
+        <a href={poster} target="_blank">
+          Poster
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Discord;
