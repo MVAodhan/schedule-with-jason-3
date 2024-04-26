7cb5e8c59b6b9edd743967eac7b1833adf25fa78
Aodhan Hamilton <hamilton.aodhan@gmail.com>
2024-04-26 16:48:39 +1200

    fix cache on netlifys side i think

--- app/api/episodes/route.ts
+++ app/api/episodes/route.ts
@@ -1,7 +1,9 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
+export async function GET(request: Request, res: NextResponse) {
+
+	res.headers.set('Cache-Control', 'no-store')
 	// const prisma = new PrismaClient();
 	const episodes = await prisma.episode.findMany({
 		orderBy: {
