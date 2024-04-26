ddb8292c8a4c5357435e82ffb03a10e3618f792f
Aodhan Hamilton <hamilton.aodhan@gmail.com>
2024-04-26 17:43:12 +1200

    does it work now

--- next.config.js
+++ next.config.js
@@ -1,21 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: "/api/episodes",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-store",
-          },
-        ],
-      },
-    ];
-  },
-};
-
 const nextConfig = {
   images: {
     loader: "custom",
