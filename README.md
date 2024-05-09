86356044f29edd43a495548369ce505f2e3ef7a5
Aodhan Hamilton <hamilton.aodhan@gmail.com>
2024-05-09 17:32:38 +1200

    update tags copy function

--- README.md
+++ README.md
@@ -3,9 +3,3 @@ This is an internal app for scheduling [Learn with Jason episodes]("https://www.
 It is an App to aggregate various information from accross the web to schedule episodes.
 
 It is a Next.Js app, with Clerk Auth and a Postgres database hosted on Railway.
-
-
-
-
-
-
--- components/edit-components/youtube/publishing.tsx
+++ components/edit-components/youtube/publishing.tsx
@@ -5,74 +5,72 @@ import { Episode } from "@prisma/client";
 import { useEffect, useState } from "react";
 import { VscCopy } from "react-icons/vsc";
 type Tag = {
-	label: string;
-	slug: string;
-	uri: string;
+  label: string;
+  slug: string;
+  uri: string;
 };
 type Link = {
-	id: string;
-	value: string;
+  id: string;
+  value: string;
 };
 
 const Publishing = ({ episode }: { episode: Episode }) => {
-	const [JSONLinks, setJSONLinks] = useState<Link[] | null>();
-	const copyText = (text: string) => {
-		navigator.clipboard.writeText(text);
-	};
+  const [JSONLinks, setJSONLinks] = useState<Link[] | null>();
+  const copyText = (text: string) => {
+    navigator.clipboard.writeText(text);
+  };
 
-	useEffect(() => {
-		if (episode.links) {
-			const linksArray = episode.links as Link[];
-			setJSONLinks(linksArray);
-		}
+  useEffect(() => {
+    if (episode.links) {
+      const linksArray = episode.links as Link[];
+      setJSONLinks(linksArray);
+    }
 
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-	const getTags = () => {
-		let json;
-		if (episode.tags) {
-			json = JSON.parse(episode?.tags as string);
-		}
-		const tagsArray = json.map((tag: Tag) => tag.label);
-		let tags: string = "";
-		if (tagsArray.length > 1) {
-			for (let tag of tagsArray) {
-				tags = tags + tag + ",";
-			}
-		} else {
-			tags = tagsArray[0];
-		}
+  const getTags = () => {
+    let tagsArray;
+    if (episode.tags) {
+      tagsArray = episode.tags!.map((tag: Tag) => tag.label);
+      let tags: string = "";
+      if (tagsArray.length > 1) {
+        for (let tag of tagsArray) {
+          tags = tags + tag + ",";
+        }
+      } else {
+        tags = tagsArray[0];
+      }
+    }
 
-		console.log(tags);
-		return tags;
-	};
+    return tags;
+  };
 
-	const formatLinks = () => {
-		if (JSONLinks && JSONLinks.length > 1) {
-			let linkSet = new Set();
-			let linkValues = JSONLinks.map(
-				(link: Link) => `- ${link.value}`
-			) as String[];
-			for (let value of linkValues) {
-				linkSet.add(value);
-			}
-			// console.log(linkSet);
-			let linksString: any = [];
-			linkSet.forEach((link) => {
-				if (link !== "- https://www.learnwithjason.dev/schedule") {
-					linksString = [...linksString, link];
-				}
-			});
-			// console.log(linksString);
-			let uniqueStrings = linksString.join("\n");
+  const formatLinks = () => {
+    if (JSONLinks && JSONLinks.length > 1) {
+      let linkSet = new Set();
+      let linkValues = JSONLinks.map(
+        (link: Link) => `- ${link.value}`
+      ) as String[];
+      for (let value of linkValues) {
+        linkSet.add(value);
+      }
+      // console.log(linkSet);
+      let linksString: any = [];
+      linkSet.forEach((link) => {
+        if (link !== "- https://www.learnwithjason.dev/schedule") {
+          linksString = [...linksString, link];
+        }
+      });
+      // console.log(linksString);
+      let uniqueStrings = linksString.join("\n");
 
-			return uniqueStrings;
-		}
-		return "No links found";
-	};
+      return uniqueStrings;
+    }
+    return "No links found";
+  };
 
-	const youtubeDescription = `
+  const youtubeDescription = `
 ${episode.description}
 
 ${episode.demo ? `Demo ${episode.demo}` : ""}
@@ -90,41 +88,41 @@ ${getCredits()}
 
 ${episode.chapters ?? `Chapters: ${episode.chapters}`}`;
 
-	return (
-		<div className="flex flex-col items-center w-3/5 m-5">
-			<div className="w-full flex flex-col items-center">
-				<div className="flex flex-row">
-					<label className="label">Title</label>
-					<VscCopy
-						className="cursor-pointer pl-1 h-8 w-8"
-						onClick={() => {
-							copyText(episode.title);
-						}}
-					/>
-				</div>
-			</div>
-			<div className="flex items-center mt-5">
-				<label>Youtube Description</label>
-				<VscCopy
-					className="cursor-pointer pl-1 h-8 w-8"
-					// copyText(youtubeDescription);
-					onClick={() => {
-						copyText(youtubeDescription);
-					}}
-				/>
-			</div>
+  return (
+    <div className="flex flex-col items-center w-3/5 m-5">
+      <div className="w-full flex flex-col items-center">
+        <div className="flex flex-row">
+          <label className="label">Title</label>
+          <VscCopy
+            className="cursor-pointer pl-1 h-8 w-8"
+            onClick={() => {
+              copyText(episode.title);
+            }}
+          />
+        </div>
+      </div>
+      <div className="flex items-center mt-5">
+        <label>Youtube Description</label>
+        <VscCopy
+          className="cursor-pointer pl-1 h-8 w-8"
+          // copyText(youtubeDescription);
+          onClick={() => {
+            copyText(youtubeDescription);
+          }}
+        />
+      </div>
 
-			<div className="flex items-center mt-5">
-				<label>Youtube Tags</label>
-				<VscCopy
-					className="cursor-pointer pl-1 h-8 w-8"
-					onClick={() => {
-						copyText(getTags());
-					}}
-				/>
-			</div>
-		</div>
-	);
+      <div className="flex items-center mt-5">
+        <label>Youtube Tags</label>
+        <VscCopy
+          className="cursor-pointer pl-1 h-8 w-8"
+          onClick={() => {
+            copyText(getTags());
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Publishing;
