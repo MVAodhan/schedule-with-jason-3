fd7f3c2dfe4588a4190fdb03b7bf00675a428d63
Aodhan Hamilton <hamilton.aodhan@gmail.com>
2024-05-15 23:25:08 +1200

    remove random log button and update reset api

--- app/api/episode/[id]/reset/route.ts
+++ app/api/episode/[id]/reset/route.ts
@@ -4,19 +4,21 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req : NextRequest, res : NextResponse){
 
     const body = await req.json()
-    
 			await prisma.episode.update({
 				where: {
 					id: body.id
 				},
 				data: {
                     date: '',
-					yt_live_link : ''
+					yt_live_link : '',
+					schedule_tweet : false,
+					ninety_minute_tweet: false,
+					live_tweet: false,
+					discord_event: false
 				},
 			});
-			prisma.$disconnect();
-    
+	prisma.$disconnect();
     return NextResponse.json({
-        message: 'successfulreset '
+        message: 'successful reset '
     })
 }
